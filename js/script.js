06e2c31acfb580f35a81bbaa4f052345a0f7238c
Nippu kumari shah <148081990+Nippukumarishah@users.noreply.github.com>
2024-01-21 17:26:12 +0530

    adding the resposive portfolio

--- js/script.js
+++ js/script.js
@@ -34,6 +34,19 @@ let changeText =()=>{
 changeText();
 setInterval(changeText, 3000)
 
+/* ======active menu =====*/
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove('active'));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener('scroll',activeMenu);
+
 /* ====== email ====== */
 const contactForm =document.getElementById('contact-form'),
       contactName =document.getElementById('contact-name'),
@@ -68,4 +81,21 @@ if(contactName.value === '' || contactEmail.value === '' || contactProject.value
 }
 contactForm.addEventListener('submit',sendEmail)
 
+/* ==== sticky navbar ==== */
+const header =document.querySelector('header');
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+/* === toggle icon navbar ===*/
+let menuIcon = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+window.onscroll = () => {
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
 
